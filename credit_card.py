2c13b0a3913278b42a82ec172731746bd4d67b26
Paul Morales <edpamogu@gmail.com>
2024-03-19 02:13:03 -0500

    Version 1

---
+++ credit_card.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 15 15:40:16 2024
+
+@author: ALONSO ESPEJO
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+base_application = pd.read_csv("C:/Users/ALONSO ESPEJO/Desktop/Mmmmorales/Credit card/application_record.csv/application_record.csv")
+base_credit_record = pd.read_csv("C:/Users/ALONSO ESPEJO/Desktop/Mmmmorales/Credit card/application_record.csv/credit_record.csv")
+
